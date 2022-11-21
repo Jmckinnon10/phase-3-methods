a332189780163c9209de918e0284c18134066104
John McKinnon <johnmckinnon23@yahoo.com>
2022-11-20 20:41:17 -0700

    commit tree d6fc29871d65d2cba8cc4fc6580d0625cffb34fd

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+ puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name = "Programmer")
+  puts "Hello, #{name}!"
+
+end
+
+greet_with_default
+
+def add(num1, num2)
+
+  num1 + num2
+
+end 
+add(2,5)
+
+def halve(num)
+  num / 2
+end
+
+halve(6)
